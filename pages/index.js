ba9f446f10dd296f8f82a15139854dc840818e35
joaojuniorbass <joaojuniorbass@yahoo.com.br>
2025-02-14 18:45:40 +0000

    Atualização da página

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 function Home() {
-    return <h1>ESSE É O ANO DA SUA MUDANÇA, JOÃO. CREIA!!</h1>
+    return <h1>ESSE É O ANO DA SUA MUDANÇA. CREIA!! Versionamento, Deploy!!
+    </h1>
 }
 
 export default Home;
